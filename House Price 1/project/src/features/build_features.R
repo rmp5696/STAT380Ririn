f9d346bacf4836019a5105f764caac2d6c0cba17
Eerini <Eerini@users.noreply.github.com>
2024-09-05 14:43:09 -0400

    .

--- House Price 1/project/src/features/build_features.R
+++ House Price 1/project/src/features/build_features.R
@@ -1,1 +1,6 @@
 #Data Wrangling
+
+#Reading csv files.
+#REMEMBER TO USE gsub FUNCTION
+
+raw_house <- read.csv("./project/volume/data/raw/Stat_380_housedata.csv")
---
+++ House Price 1/run_project.R
@@ -0,0 +1,2 @@
+source("project/src/features/build_features.R")
+source("project/src/models/train_model.R")
